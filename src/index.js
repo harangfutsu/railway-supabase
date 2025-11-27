1fbb160179dbbd5135150bdb4a16a0461468e1e5
budiwputra@gmail.com <budiwputra@gmail.com>
2025-11-27 22:51:53 +0700

    add cors origin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 const app = express()
 
 const routes = require('./routes')
 const config = require('./config')
 
 app.enable('trust proxy')
+
+app.use(cors({
+    origin: "https://fe-videobelajar-react.vercel.app", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
